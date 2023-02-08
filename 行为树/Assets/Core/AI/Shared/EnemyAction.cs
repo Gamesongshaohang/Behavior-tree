76dd3396bded4d2fca11b44377537c0eea0c9782
Gamesongshaohang <1026217118@qq.com>
2023-02-08 18:12:55 +0800

    行为树第一章

---
+++ 行为树/Assets/Core/AI/Shared/EnemyAction.cs
@@ -0,0 +1,25 @@
+﻿using BehaviorDesigner.Runtime.Tasks;
+using Core.Character;
+using Core.Combat;
+using UnityEngine;
+
+namespace Core.AI
+{
+    public class EnemyAction : Action
+    {
+        protected Rigidbody2D body;
+        protected Animator animator;
+        protected Destructable destructable;
+        protected PlayerController player;
+        protected SpriteRenderer spriteRenderer;
+        
+        public override void OnAwake()
+        {
+            body = GetComponent<Rigidbody2D>();
+            player = PlayerController.Instance;
+            destructable = GetComponent<Destructable>();
+            animator = gameObject.GetComponentInChildren<Animator>();
+            spriteRenderer =  gameObject.GetComponentInChildren<SpriteRenderer>();
+        }
+    }
+}
